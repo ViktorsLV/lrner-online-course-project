037ad6cb11a05cebfcec762c9e428b6e9370d6d2
ViktorsLV <viktors.ch11@gmail.com>
2022-05-31 14:27:56 +0200

    Profile page navigation setup

---
+++ client/src/components/Profile/ProfileNavigation.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const ProfileNavigation = ({ avatar, firstName, lastName }) => {
+    const tabActive = 'text-accent-500 font-bold border-b-4 border-accent-500'
+
+    const pages = [
+        { id: 1, url: 'my-profile', text: 'My Profile' },
+        { id: 2, url: 'order-history', text: 'Order History' },
+        { id: 3, url: 'delete-profile', text: 'Delete Profile' },
+    ]
+
+    return (
+        <div className='flex flex-col justify-center items-center p-5'>
+            <div className='flex flex-col items-center mb-20 space-y-5'>
+                <img src={avatar} alt={firstName} className="w-2/3 rounded-full" />
+                <h3 className='text-md'>{firstName} {lastName}</h3>
+            </div>
+            {pages && pages.map(page => (
+                <NavLink key={page.id} to={`${page.url}`} className={({ isActive }) => isActive ? tabActive : 'hover:text-accent-500'}>
+                    <p className='text-lg '>{page.text}</p>
+                </NavLink>
+            ))}
+        </div>
+    )
+}
+
+export default ProfileNavigation
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import { LoginIcon } from '@heroicons/react/outline'
 import { Store } from "../utils/Store";
-import { NavLink, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import ProfileNavigation from "../components/Profile/ProfileNavigation";
 
 const Profile = () => {
     const [user, setUser] = useState({})
@@ -20,33 +20,18 @@ const Profile = () => {
         setUser(userInfo)
     }, [navigate, userInfo]);
 
-    // if (isLoading || loading) {
-    //     return <div>Loading ...</div>;
-    // }
-    const tabActive = 'text-accent-500 font-bold border-b-4 border-accent-500'
-
     return (
         user && (
-            <div className="min-h-full flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-                <h1 className='text-3xl mb-4'>Profile page</h1>
-                <img src={user.picture} alt={user.family_name} />
-                <h2>{user.given_name} {user.family_name}</h2>
-                <div className='flex space-x-10 my-10'>
-                    <NavLink to='my-profile' className={({ isActive }) => isActive ? tabActive : 'hover:text-accent-500'}>
-                        <p className=''>My Profile</p>
-                    </NavLink>
-
-                    <NavLink to='order-history' className={({ isActive }) => isActive ? tabActive : 'hover:text-accent-500'}>
-                        <p className=''>Order History</p>
-                    </NavLink>
-
-                    <NavLink to='delete-profile' className={({ isActive }) => isActive ? tabActive : 'hover:text-accent-500'}>
-                        <p className=''>Delete Profile</p>
-                    </NavLink>
+            <div className="flex min-h-full py-20">
+                <div className='w-1/4'>
+                    <ProfileNavigation 
+                        avatar={user.picture}
+                        firstName={user.given_name}
+                        lastName={user.family_name}
+                    />
                 </div>
-
-                {/* Rendering children routes */}
-                <div>
+                <div className='border w-0.5 bg-black border-black'></div>
+                <div className="flex flex-col items-center w-full justify-center py-12 px-4 sm:px-6 lg:px-8">
                     <Outlet />
                 </div>
             </div>
